db9c21748f378bf5eb8bc6320eac0ea29a5770f5
codefromrvk <varshithkumar553@gmail.com>
2023-07-23 23:37:13 +0530

    fix: change svg to png

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import Image from "next/image";
 import { ColorWheelIcon } from "@radix-ui/react-icons";
 
-import wheel from "public/assets/wheel.svg";
+import wheel from "public/assets/wheel.png";
 const Loader = () => {
   return (
     <div className=" flex flex-col items-center justify-center">
         <Image
           className="h-full w-full animate-spin object-contain"
           src={wheel}
-        //   src="/assets/wheel.svg"
           alt="Loading"
           width={100} //automatically provided
           height={100} //automatically provided
