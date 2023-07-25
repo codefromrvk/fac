7d0821ae25a37c0f2a446db766ba8c24d994f77a
R Varshith Kumar <72021268+codefromrvk@users.noreply.github.com>
2023-07-25 22:32:57 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title:
-    "Friends Auto Car | Sell Car in Mangalore | Quality Second-Hand Cars | Friends Auto Cars | Best Sellers",
+    "Friends Auto Car | Quality Second-Hand Cars ",
   description:
-    "Looking to buy or sell a car in Mangalore? Discover a wide selection of quality second-hand cars at Friends Auto Cars, one of the best car sellers in the area. Trusted dealers offering top-notch vehicles and exceptional customer service. Get a great price for your used car today! A true OLX alternative",
+    "Looking to buy or sell a car in Mangalore? Discover a wide selection of quality second-hand cars at Friends Auto Cars, one of the best car sellers in the area.",
 };
 
 export default function RootLayout({
@@ -26,6 +26,10 @@ export default function RootLayout({
           name="ahrefs-site-verification"
           content="b50078f15f79994fc28290237a5ce6268035f3cc15b0b5fc787373398519c174"
         />
+           <meta
+      name="keywords"
+      content="Indian news, news app, latest headlines,India, India news, world news, breaking news"
+    />
       </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
