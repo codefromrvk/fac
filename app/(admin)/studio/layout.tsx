b34ca2651d44d36ae6bc3aaab648ede56ab07dba
codefromrvk <varshithkumar553@gmail.com>
2023-07-25 22:57:39 +0530

    feat: add sanity

---
+++ app/(admin)/studio/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Admin ',
+  description: 'CMS',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
